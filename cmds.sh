80c8e5028365dc82b7ae0df1244fc39768ba95f3
Tom Strempel <tom.strempel@ufz.de>
2021-05-11 15:56:34 +0200

    updated script so that data is written to 3 different files

--- cmds.sh
+++ cmds.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # get monitoring data
-( /home/tstrempel/git/scaphandre/target/release/scaphandre --no-header json -t 10 > energy_data.json ; for i in {1..10}; do cat /proc/loadavg ; sleep 1; done  | awk -e '{print $1}' > load_average.txt ) | parallel
+( /home/tstrempel/git/scaphandre/target/release/scaphandre --no-header json -t 10 > energy_data.json ; for i in {1..10}; do date +"%T.%3N"; cat /proc/loadavg ; sleep 1; done > load_average.txt ) | parallel
+
+/home/tstrempel/git/scaphandre/target/release/scaphandre --no-header json -t 10 > energy_data.json &
+for i in {1..10}; do date +"%T.%3N"; sleep 1; done > time.txt &
+for i in {1..10}; do date +"%T.%3N"; cat /proc/loadavg ; sleep 1; done > load_average.txt &
 
 # get socket consumption
 jq '.sockets[] | .consumption' energy_data.json

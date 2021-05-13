63ec08417873182d3e8570269fcda6a516f5d2cc
Tom Strempel <tom.strempel@ufz.de>
2021-05-14 00:19:59 +0200

    use extra file for energy measurements

--- cmds.sh
+++ cmds.sh
@@ -3,12 +3,12 @@
 # get monitoring data
 # ( /home/tstrempel/git/scaphandre/target/release/scaphandre --no-header json -t 10 > energy_data.json ; for i in {1..10}; do date +"%T.%3N"; cat /proc/loadavg ; sleep 1; done > load_average.txt ) | parallel
 
-timeout=100
+timeout=10
 step=1
 
 # get monitoring data
 # energy data, timestamps, load average, system stat (cpu util etc.)
-/home/tstrempel/git/scaphandre/target/release/scaphandre --no-header json -t $timeout -s $step > data/energy_data.json &
+/home/tstrempel/git/scaphandre/target/release/scaphandre --no-header json -t $timeout -s 0 -n 200000000 > data/energy_data.json &
 for ((i=1; i<=$timeout; i++)); do date +"+%s%3N"; sleep $step; done > data/time.txt &
 for ((i=1; i<=$timeout; i++)); do cat /proc/loadavg; sleep $step; done > data/load_average.txt &
 # vmstat $step $timeout > data/sys.txt &
@@ -21,9 +21,13 @@ sleep 1
 jq '.sockets[] | .consumption' data/energy_data.json > data/energy_data.txt
 awk -e '{print $1}' < data/load_average.txt > data/load_average2.txt
 sed -e '1,3d' < data/sys.txt | sed -e '$ d' | awk -e '{print $13}' > data/sys2.txt
+paste -d ',' data/time.txt data/sys2.txt data/load_average2.txt > data/system_data.csv
 
-paste -d ',' data/time.txt data/energy_data.txt data/sys2.txt data/load_average2.txt > data/all.csv
-rm data/time.txt data/energy_data.txt data/sys.txt data/sys2.txt data/load_average.txt data/load_average2.txt
+first=$(head -n 1 data/time.txt)
+for ((i=0; i<$((5 * $timeout)); i++)); do echo $(($first + i * 100)); done > data/time_data.txt
+paste -d ',' data/time_data.txt data/energy_data.txt
+# rm data/time.txt data/energy_data.txt data/sys.txt data/sys2.txt data/load_average.txt data/load_average2.txt
+
 
 # 1 min load average
 # cat /proc/loadavg | awk -e '{print $1}' > load_average.txt

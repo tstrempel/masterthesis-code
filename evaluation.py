3a0a07368bb0a86b8f6696f8221e207d2d1f0851
Tom Strempel <tom.strempel@ufz.de>
2021-05-16 23:42:29 +0200

    fix python script

--- evaluation.py
+++ evaluation.py
@@ -7,31 +7,26 @@ import json
 
 from pandas.core.frame import DataFrame
 
+# read in JSON as text as python json can't parse multiple JSON objects at a time
 with open('data/energy_data_beautified.json') as json_file:
     data = json_file.read()
-# print(jq.compile(".host").input(text=data).text())
 
-# df = pd.json_normalize(json.loads(jq.compile(".host").input(text=data).first()))
-# df = pd.read_json(jq.compile(".host").input(text=data).first())
-
-df = pd.DataFrame(columns = ['timestamp', 'socket_power'])
-
+energy_data = pd.DataFrame(columns = ['timestamp', 'socket_power'])
+# iterate over JSON objects to fill the dataframe
 iterator = iter(jq.compile(".host").input(text=data))
 for item in iterator:
     new_row = {'timestamp': item['timestamp'], 'socket_power': item['consumption']}
-    print(new_row)
-    df = df.append(new_row, ignore_index=True)
+    energy_data = energy_data.append(new_row, ignore_index=True)
 
 
-energy_data = pd.read_csv("data/energy_data.csv")
 energy_data['timestamp'] = energy_data['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
 energy_data['socket_power'] = energy_data['socket_power'].apply(lambda x: x/1000000.0)
-energy_data['app_power'] = energy_data['app_power'].apply(lambda x: x/1000000.0)
+# energy_data['app_power'] = energy_data['app_power'].apply(lambda x: x/1000000.0)
 
-plt.plot(energy_data['timestamp'], energy_data['app_power'])
+# plt.plot(energy_data['timestamp'], energy_data['app_power'])
 plt.plot(energy_data['timestamp'], energy_data['socket_power'])
 plt.xlabel("Timestamp")
 plt.ylabel("Socket power consumption in Watt")
 locs, labels = plt.xticks()
 plt.xticks([energy_data['timestamp'][1], energy_data['timestamp'][len(energy_data)-1]])
-# plt.show()
+plt.show()

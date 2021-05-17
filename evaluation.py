5a320683fbbbc7862ca6abffece3bf89f0fcac7f
Tom Strempel <tom.strempel@ufz.de>
2021-05-17 08:22:57 +0200

    prepare evaluation.py for restructuring

--- evaluation.py
+++ evaluation.py
@@ -5,8 +5,6 @@ from datetime import datetime
 import jq
 import json
 
-from pandas.core.frame import DataFrame
-
 # read in JSON as text as python json can't parse multiple JSON objects at a time
 with open('data/energy_data_beautified.json') as json_file:
     data = json_file.read()
@@ -14,15 +12,17 @@ with open('data/energy_data_beautified.json') as json_file:
 energy_data = pd.DataFrame(columns = ['timestamp', 'socket_power'])
 # iterate over JSON objects to fill the dataframe
 iterator = iter(jq.compile(".host").input(text=data))
+power_iterator = iter(jq.compile(".sockets[]").input(text=data))
+
 for item in iterator:
     new_row = {'timestamp': item['timestamp'], 'socket_power': item['consumption']}
     energy_data = energy_data.append(new_row, ignore_index=True)
-
-
 energy_data['timestamp'] = energy_data['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
 energy_data['socket_power'] = energy_data['socket_power'].apply(lambda x: x/1000000.0)
+
 # energy_data['app_power'] = energy_data['app_power'].apply(lambda x: x/1000000.0)
 
+print(len(energy_data))
 # plt.plot(energy_data['timestamp'], energy_data['app_power'])
 plt.plot(energy_data['timestamp'], energy_data['socket_power'])
 plt.xlabel("Timestamp")

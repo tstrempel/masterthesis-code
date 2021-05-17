6e0b89ca70ec61ef8c421c82027f0e3134a5b863
Tom Strempel <tom.strempel@ufz.de>
2021-05-17 09:23:55 +0200

    add core, uncore and dram metrics to plot

--- evaluation.py
+++ evaluation.py
@@ -9,24 +9,31 @@ import json
 with open('data/energy_data_beautified.json') as json_file:
     data = json_file.read()
 
-energy_data = pd.DataFrame(columns = ['timestamp', 'socket_power'])
+energy_data = pd.DataFrame(columns = ['timestamp', 'socket_power', 'core', 'uncore', 'dram'])
 # iterate over JSON objects to fill the dataframe
 iterator = iter(jq.compile(".host").input(text=data))
 power_iterator = iter(jq.compile(".sockets[]").input(text=data))
 
-for item in iterator:
-    new_row = {'timestamp': item['timestamp'], 'socket_power': item['consumption']}
+for item,power_item in zip(iterator, power_iterator):
+    new_row = {'timestamp': item['timestamp'], 'socket_power': power_item['consumption'], 'core': power_item['domains'][0]['consumption'], 'uncore': power_item['domains'][1]['consumption'], 'dram': power_item['domains'][2]['consumption']}
     energy_data = energy_data.append(new_row, ignore_index=True)
+
 energy_data['timestamp'] = energy_data['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
 energy_data['socket_power'] = energy_data['socket_power'].apply(lambda x: x/1000000.0)
-
-# energy_data['app_power'] = energy_data['app_power'].apply(lambda x: x/1000000.0)
+energy_data['core'] = energy_data['core'].apply(lambda x: x/1000000.0)
+energy_data['uncore'] = energy_data['uncore'].apply(lambda x: x/1000000.0)
+# energy_data['dram'] = energy_data['socket_power'] - energy_data['core'] - energy_data['uncore']  # dram bug fix by calculating it
+energy_data['dram'] = energy_data['dram'].apply(lambda x: x/1000000.0)
 
 print(len(energy_data))
-# plt.plot(energy_data['timestamp'], energy_data['app_power'])
-plt.plot(energy_data['timestamp'], energy_data['socket_power'])
+plt.plot(energy_data['timestamp'], energy_data['socket_power'], label="Socket power consumption")
+plt.plot(energy_data['timestamp'], energy_data['core'], label="Core power consumption")
+plt.plot(energy_data['timestamp'], energy_data['uncore'], label="Uncore power consumption")
+plt.plot(energy_data['timestamp'], energy_data['dram'], label="DRAM power consumption")
 plt.xlabel("Timestamp")
 plt.ylabel("Socket power consumption in Watt")
 locs, labels = plt.xticks()
-plt.xticks([energy_data['timestamp'][1], energy_data['timestamp'][len(energy_data)-1]])
+plt.xticks([energy_data['timestamp'][0], energy_data['timestamp'][len(energy_data)-1]])
+plt.legend()
+plt.grid()
 plt.show()

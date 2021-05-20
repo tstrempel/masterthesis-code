55d68d22813f8c29850cec1434ba296a3f72adef
Tom Strempel <tom.strempel@ufz.de>
2021-05-20 09:41:54 +0200

    remove unncessary commads from cmds.sh

--- cmds.sh
+++ cmds.sh
@@ -19,21 +19,13 @@ mpstat $step $timeout > data/sys.txt &
 sleep $timeout
 sleep 1
 
-# process monitoring datai
-jq '.sockets[] | .consumption' data/energy_data.json > data/energy_data.txt
-jq '.host.timestamp' data/energy_data.json > data/time_data.txt
-jq '.consumers[] | select(.exe=="/usr/share/code/code") | .consumption' data/energy_data.json > data/app.txt
-# paste -d ',' "$(jq '. | select(.consumers[].exe=="/usr/share/code/code") | .host.timestamp' data/energy_data.json)" "$(jq '.consumers[] | select(.exe=="/usr/share/code/code") | .consumption' data/energy_data.json)" > app.csv
+# process monitoring data
 
 awk -e '{print $1}' < data/load_average.txt > data/load_average2.txt
 sed -e '1,3d' < data/sys.txt | sed -e '$ d' | awk -e '{print $13}' > data/sys2.txt
 echo "timestamp,socket_idle,load_average" > data/system_data.csv
 paste -d ',' data/time.txt data/sys2.txt data/load_average2.txt >> data/system_data.csv
 
-# first=$(head -n 1 data/time.txt)
-# for ((i=0; i<$((2 * $timeout)); i++)); do echo $(($first + i * 500)); done > data/time_data.txt
-echo "timestamp,socket_power,app_power" > data/energy_data.csv
-paste -d ',' data/time_data.txt data/energy_data.txt data/app.txt >> data/energy_data.csv
 # rm data/time.txt data/energy_data.txt data/sys.txt data/sys2.txt data/load_average.txt data/load_average2.txt
 
 js-beautify data/energy_data.json > data/energy_data_beautified.json

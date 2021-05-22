2a5632c0280a9087c114823adba73f2f27146b38
Tom Strempel <tom.strempel@ufz.de>
2021-05-22 13:56:11 +0200

    status before trying to get only a number of ticks

--- evaluation.py
+++ evaluation.py
@@ -35,80 +35,85 @@ def process_socket_energy_data(data):
 
     return energy_data
 
+def process_system_metrics(system_data):
+    system_data['timestamp'] = system_data['timestamp'].apply(lambda x: x / 1000.0)
+    system_data['timestamp'] = system_data['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
+    system_data['socket_idle'] = system_data['socket_idle'].apply(lambda x: 1 - (x / 100))
+    return system_data
+
+def process_app_metrics(data):
+    app_set = set()
+    apps = iter(jq.compile(".consumers[].exe").input(text=data))
+    for item in apps:
+        if item not in app_set and not item == "":
+            app_set.add(item)
+
+    app_dict = dict()
+    df_app_power = pd.DataFrame(columns=['app_name', 'consumption'])
+    for app in app_set:
+        df_tmp = pd.DataFrame(columns = ['timestamp', 'consumption'])
+        for app_result in iter(jq.compile('select(.consumers[].exe=="' + app + '")').input(text=data)):
+            res = next((sub for sub in app_result['consumers'] if sub['exe'] == app), None)
+            new_row = {'timestamp': app_result['host']['timestamp'], 'consumption': res['consumption']}
+            df_tmp = df_tmp.append(new_row, ignore_index=True)
+        df_tmp.drop_duplicates(keep='first', inplace=True)
+        df_tmp['timestamp'] = df_tmp['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
+        df_tmp['consumption'] = df_tmp['consumption'].apply(lambda x: x/1000000.0)
+
+        app_name = os.path.basename(os.path.normpath(app))
+        app_dict[app_name] = df_tmp
+        df_app_power = df_app_power.append({'app_name': app_name, 'consumption': sum(df_tmp['consumption'])}, ignore_index=True)
+
+    df_app_power = df_app_power.sort_values('consumption', ascending=False)
+    df_app_power = df_app_power.reset_index(drop=True)
+    return app_dict, df_app_power
+
+def plot_biggest_consumers():
+    return None
+
 data = read_in_scaphandre_json_file(sys.argv[1])
 system_data = read_in_system_csv(sys.argv[2])
 
 energy_data = process_socket_energy_data(data)
+system_data = process_system_metrics(system_data)
+print("energy data")
 print(energy_data)
+print("system data")
+print(system_data)
 
+# fig, ax = plt.subplots()
 plt.figure("Energy data")
 plt.plot(energy_data['timestamp'], energy_data['socket_power'], label="Socket power consumption")
 plt.plot(energy_data['timestamp'], energy_data['core'], label="Core power consumption")
 plt.plot(energy_data['timestamp'], energy_data['uncore'], label="Uncore power consumption")
 plt.plot(energy_data['timestamp'], energy_data['dram'], label="DRAM power consumption")
-
 plt.xlabel("Timestamp")
 plt.ylabel("Power consumption in Watt")
 # locs, labels = plt.xticks()
-plt.xticks([energy_data['timestamp'][0], energy_data['timestamp'][len(energy_data)-1]])
+# plt.xticks([energy_data['timestamp'][0], energy_data['timestamp'][len(energy_data)-1]])
+# plt.gca().locator_params(axis='x', nbins=10)
+plt.locator_params(axis='x', nbins=10)
 plt.legend()
 plt.grid()
 
-# plt.figure("App data")
-# plt.plot(energy_data['timestamp'], energy_data['socket_power'], label="Socket power consumption")
-# plt.plot(df_app['timestamp'], df_app['consumption'], label="VS Code power consumption")
-# plt.legend()
-# plt.grid()
+fig, ax = plt.subplots()
+# plt.figure("System data")
+ax.scatter(system_data['timestamp'], system_data['socket_idle'], label="Non Idle")
+ax.scatter(system_data['timestamp'], system_data['load_average'], label="Load Average")
+ax.scatter(energy_data['timestamp'], energy_data['socket_power'], label="Socket power consumption")
+# plt.xticks([energy_data['timestamp'][0], energy_data['timestamp'][len(energy_data-1)], system_data['timestamp'][0], system_data['timestamp'][len(system_data)-1]])
+#plt.xticks([energy_data['timestamp'][0], system_data['timestamp'][0]])
+#plt.legend()
+ax.grid(True)
 
 
-system_data['timestamp'] = system_data['timestamp'].apply(lambda x: x / 1000.0)
-system_data['timestamp'] = system_data['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
-system_data['socket_idle'] = system_data['socket_idle'].apply(lambda x: 1 - (x / 100))
-
-plt.figure("System data")
-plt.plot(system_data['timestamp'], system_data['socket_idle'], label="Non Idle")
-plt.plot(system_data['timestamp'], system_data['load_average'], label="Load Average")
-plt.plot(energy_data['timestamp'], energy_data['socket_power'], label="Socket power consumption")
-plt.xticks([system_data['timestamp'][0], system_data['timestamp'][len(system_data)-1]])
-plt.legend()
-plt.grid()
-
-print(energy_data)
-print(system_data)
-
-# plt.show()
-
-app_set = set()
-apps = iter(jq.compile(".consumers[].exe").input(text=data))
-for item in apps:
-    if item not in app_set and not item == "":
-        app_set.add(item)
-
-
-app_dict = dict()
-df_app_power = pd.DataFrame(columns=['app_name', 'consumption'])
-for app in app_set:
-    df_tmp = pd.DataFrame(columns = ['timestamp', 'consumption'])
-    for app_result in iter(jq.compile('select(.consumers[].exe=="' + app + '")').input(text=data)):
-        res = next((sub for sub in app_result['consumers'] if sub['exe'] == app), None)
-        new_row = {'timestamp': app_result['host']['timestamp'], 'consumption': res['consumption']}
-        df_tmp = df_tmp.append(new_row, ignore_index=True)
-    df_tmp.drop_duplicates(keep='first', inplace=True)
-    df_tmp['timestamp'] = df_tmp['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
-    df_tmp['consumption'] = df_tmp['consumption'].apply(lambda x: x/1000000.0)
-
-    app_name = os.path.basename(os.path.normpath(app))
-    app_dict[app_name] = df_tmp
-    df_app_power = df_app_power.append({'app_name': app_name, 'consumption': sum(df_tmp['consumption'])}, ignore_index=True)
-
-df_app_power = df_app_power.sort_values('consumption', ascending=False)
-df_app_power = df_app_power.reset_index(drop=True)
+app_dict, df_app_power = process_app_metrics(data)
 
 plt.figure("Most energy intesive applications")
 
 for i in range(1, 5):
     plt.plot(app_dict[df_app_power.iloc[i]['app_name']]['timestamp'], app_dict[df_app_power.iloc[i]['app_name']]['consumption'], label=df_app_power.iloc[i]['app_name'])
-plt.xticks([system_data['timestamp'][0], system_data['timestamp'][len(system_data)-1]])
+plt.xticks([energy_data['timestamp'][0], energy_data['timestamp'][len(energy_data)-1]])
 plt.legend()
 plt.grid()
 

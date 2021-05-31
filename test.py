3e52c4b7cbd111f1a3aab18665d959032a6e2d35
Tom Strempel <tom.strempel@ufz.de>
2021-05-31 22:34:23 +0200

    delete old files

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import json
-import pandas as pd
-from pprint import pprint
-
-with open('res.json', 'r') as f:
-    pprint(pd.read_json(json.dumps(json.load(f)), orient='records'))

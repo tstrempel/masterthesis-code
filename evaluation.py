862fdbb8849aeb4bc1a3efe3b827ee85364d8724
Tom Strempel <tom.strempel@ufz.de>
2021-05-31 22:38:50 +0200

    replaced socket_power with consumption

--- evaluation.py
+++ evaluation.py
@@ -19,21 +19,21 @@ def read_in_system_csv(file):
     return pd.read_csv(file)
 
 def process_socket_energy_data(data):
-    energy_data = pd.DataFrame(columns = ['timestamp', 'socket_power', 'core', 'uncore', 'dram', 'average_load', 'cpu_load'])
+    energy_data = pd.DataFrame(columns = ['timestamp', 'consumption', 'core', 'uncore', 'dram', 'average_load', 'cpu_load'])
     # iterate over JSON objects to fill the dataframe
     iterator = iter(jq.compile(".host").input(text=data))
     power_iterator = iter(jq.compile(".sockets[]").input(text=data))
 
     # TODO: rewrite for usage with multi-socket systems
     for item,power_item in zip(iterator, power_iterator):
-        new_row = {'timestamp': item['timestamp'], 'socket_power': item['consumption'], 'core': power_item['domains'][0]['consumption'], 'uncore': power_item['domains'][1]['consumption'], 'dram': power_item['domains'][2]['consumption'], 'average_load': item['average_load'], 'cpu_load': item['cpu_load']}
+        new_row = {'timestamp': item['timestamp'], 'consumption': item['consumption'], 'core': power_item['domains'][0]['consumption'], 'uncore': power_item['domains'][1]['consumption'], 'dram': power_item['domains'][2]['consumption'], 'average_load': item['average_load'], 'cpu_load': item['cpu_load']}
         energy_data = energy_data.append(new_row, ignore_index=True)
     
     energy_data['timestamp'] = energy_data['timestamp'].apply(lambda time: datetime.utcfromtimestamp(time).strftime('%H:%M:%S.%f')[:-3])
-    energy_data['socket_power'] = energy_data['socket_power'].apply(lambda x: x/1000000.0)
+    energy_data['consumption'] = energy_data['consumption'].apply(lambda x: x/1000000.0)
     energy_data['core'] = energy_data['core'].apply(lambda x: x/1000000.0)
     energy_data['uncore'] = energy_data['uncore'].apply(lambda x: x/1000000.0)
-    # energy_data['dram'] = energy_data['socket_power'] - energy_data['core'] - energy_data['uncore']  # dram bug fix by calculating it
+    # energy_data['dram'] = energy_data['consumption'] - energy_data['core'] - energy_data['uncore']  # dram bug fix by calculating it
     energy_data['dram'] = energy_data['dram'].apply(lambda x: x/1000000.0)
 
     return energy_data
@@ -84,16 +84,16 @@ print(len(energy_data))
 energy_data.to_csv(r'plot/energy.csv', index=False)
 
 print("Person coefficient:")
-print(stats.pearsonr(energy_data['socket_power'], energy_data['cpu_load']))
+print(stats.pearsonr(energy_data['consumption'], energy_data['cpu_load']))
 print("Kolmogorov-Smirnov test:")
-print(stats.kstest(energy_data['socket_power'], energy_data['cpu_load']))
+print(stats.kstest(energy_data['consumption'], energy_data['cpu_load']))
 print("Kolmogorov-Smirnov 2-sample test:")
-print(stats.ks_2samp(energy_data['socket_power'], energy_data['cpu_load']))
+print(stats.ks_2samp(energy_data['consumption'], energy_data['cpu_load']))
 print("Linregress:")
-print(stats.linregress(energy_data['socket_power'], energy_data['cpu_load']))
+print(stats.linregress(energy_data['consumption'], energy_data['cpu_load']))
 
 plt.figure("Energy data")
-plt.plot(energy_data['timestamp'], energy_data['socket_power'], label="Socket power consumption")
+plt.plot(energy_data['timestamp'], energy_data['consumption'], label="Socket power consumption")
 plt.plot(energy_data['timestamp'], energy_data['core'], label="Core power consumption")
 plt.plot(energy_data['timestamp'], energy_data['uncore'], label="Uncore power consumption")
 plt.plot(energy_data['timestamp'], energy_data['dram'], label="DRAM power consumption")
@@ -109,7 +109,7 @@ plt.grid()
 plt.figure("System data")
 plt.plot(energy_data['timestamp'], energy_data['cpu_load'], label="Non Idle")
 plt.plot(energy_data['timestamp'], energy_data['average_load'], label="Average Load")
-plt.plot(energy_data['timestamp'], energy_data['socket_power'], label="Socket power consumption")
+plt.plot(energy_data['timestamp'], energy_data['consumption'], label="Socket power consumption")
 plt.xticks([energy_data['timestamp'][0], energy_data['timestamp'][len(energy_data)-1]])
 # plt.xticks([energy_data['timestamp'][0], system_data['timestamp'][0]])
 plt.legend()

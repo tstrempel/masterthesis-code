296fb1d6666f8a02f7acfbd9f5da474607d45a01
Tom Strempel <tom.strempel@ufz.de>
2021-07-21 22:58:08 +0200

    clean up cmds.sh

--- cmds.sh
+++ cmds.sh
@@ -2,13 +2,15 @@
 
 # pip install jsbeautifier
 
-timeout=30
+timeout=400
 step=1
-# scaphandre_step=500000000
+step_nano=0
+# step_nano=100000000
 
+mkdir data
 # get monitoring data
 # energy data, timestamps, load average, system stat (cpu util etc.)
-/home/tstrempel/git/scaphandre-custom/target/release/scaphandre --no-header json -t $timeout -s $step > data/energy_data.json &
+/home/tstrempel/git/scaphandre-custom/target/release/scaphandre --no-header json --timeout $timeout --step $step --step_nano $step_nano --max-top-consumers=200 > data/energy_data.json
 echo $step > data/step.txt
 
 # wait until all is done
@@ -17,4 +19,4 @@ sleep 1
 
 # process monitoring data
 js-beautify data/energy_data.json > data/energy_data_beautified.json
-python evaluation.py data/energy_data_beautified.json
+python evaluation.py data/energy_data_beautified.json 1.0

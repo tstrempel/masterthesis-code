2d062f019cc9e72f85cb7273d342dd4a5f3ad712
Tom Strempel <tom.strempel@ufz.de>
2021-08-22 12:37:10 +0200

    update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -22,6 +22,7 @@ function run_pipeline {
 }
 
 function run_time {
+    mkdir "$1"
     { time sorting "$2" "$3"; } 2> "$1/log.txt"
 }
 
@@ -70,27 +71,39 @@ function run_pipeline_vtune_nano {
 # source ~/git/scaphandre-custom/init.sh
 # source /opt/intel/oneapi/setvars.sh
 
-#mkdir selection_sort_1000 selection_sort_2000 selection_sort_4000 selection_sort_8000 selection_sort_16000 selection_sort_32000 selection_sort_64000 selection_sort_128000 selection_sort_256000
-#mkdir insertion_sort_1000 insertion_sort_2000 insertion_sort_4000 insertion_sort_8000 insertion_sort_16000 insertion_sort_32000 insertion_sort_64000 insertion_sort_128009 insertion_sort_256000
-#mkdir quick_sort_8000 quick_sort_16000 quick_sort_32000 quick_sort_64000 quick_sort_128000 quick_sort_256000 quick_sort_512000
-#mkdir qsort_8000 qsort_16000 qsort_32000 qsort_64000 qsort_128000 qsort_256000 qsort_512000
-
-#run_time selection_sort_1000 0 1000
-#run_time selection_sort_2000 0 2000
-#run_time selection_sort_4000 0 4000
-#run_pipeline_vtune selection_sort_8000 0 8000 2 1
-#run_pipeline_vtune selection_sort_16000 0 16000 5 1
-#run_pipeline_vtune selection_sort_32000 0 32000 5 1
-#run_pipeline_vtune selection_sort_64000 0 64000 5 1
+run_time selection_sort_1000 0 1000
+run_time selection_sort_2000 0 2000
+run_time selection_sort_4000 0 4000
+run_pipeline_vtune selection_sort_8000 0 8000 2 1
+run_pipeline_vtune selection_sort_16000 0 16000 5 1
+run_pipeline_vtune selection_sort_32000 0 32000 5 1
+run_pipeline_vtune selection_sort_64000 0 64000 5 1
 run_pipeline_vtune selection_sort_128000 0 128000 5 2
-# run_pipeline_vtune selection_sort_256000 0 256000 5 2
+run_pipeline_vtune selection_sort_256000 0 256000 5 2
 
-#run_time insertion_sort_1000 1 1000
-#run_time insertion_sort_2000 1 2000
-#run_time insertion_sort_4000 1 4000
-#run_pipeline_vtune insertion_sort_8000 1 8000 2 1
-#run_pipeline_vtune insertion_sort_16000 1 16000 5 1
-#run_pipeline_vtune insertion_sort_32000 1 32000 5 1
-#run_pipeline_vtune insertion_sort_64000 1 64000 5 1
+run_time insertion_sort_1000 1 1000
+run_time insertion_sort_2000 1 2000
+run_time insertion_sort_4000 1 4000
+run_pipeline_vtune insertion_sort_8000 1 8000 2 1
+run_pipeline_vtune insertion_sort_16000 1 16000 5 1
+run_pipeline_vtune insertion_sort_32000 1 32000 5 1
+run_pipeline_vtune insertion_sort_64000 1 64000 5 1
 run_pipeline_vtune insertion_sort_128000 1 128000 5 2
-# run_pipeline_vtune insertion_sort_256000 1 256000 5 2
+run_pipeline_vtune insertion_sort_256000 1 256000 5 2
+
+run_time quick_sort_32000 2 32000
+run_time quick_sort_64000 2 64000
+run_pipeline_vtune quick_sort_128000 2 128000 1 1
+run_pipeline_vtune quick_sort_256000 2 256000 1 1
+run_pipeline_vtune quick_sort_512000 2 512000 1 2
+run_pipeline_vtune quick_sort_1024000 2 1024000 1 5
+run_pipeline_vtune quick_sort_2048000 2 2048000 1 5
+
+run_time qsort_32000 3 32000 
+run_time qsort_64000 3 64000
+run_pipeline_vtune qsort_128000 3 128000 1 1
+run_pipeline_vtune qsort_256000 3 256000 1 1
+run_pipeline_vtune qsort_512000 3 512000 1 2
+run_pipeline_vtune qsort_1024000 3 1024000 1 5
+run_pipeline_vtune qsort_2048000 3 2048000 1 5
+

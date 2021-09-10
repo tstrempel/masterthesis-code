d336075cce604b39541321dc7c6e347e0e51b1b7
Tom Strempel <tom.strempel@ufz.de>
2021-09-10 18:10:52 +0200

    update evaluation_server.py with median and medium

--- evaluation_server.py
+++ evaluation_server.py
@@ -22,6 +22,15 @@ energy_data = process_socket_energy_data(data)
 
 print("Total power consumption in Joule")
 print(compute_energy_consumption(energy_data, interval))
+
+print("Median energy consumption")
+print(energy_data.median("consumption"))
+
+print("Average energy consumption")
+print(energy_data.mean("consumption"))
+
+
+
 # print(compute_total_energy_consumption(energy_data))
 
 # energy_data.to_csv(output_dir + "/energy.csv", index=False)
@@ -29,7 +38,6 @@ print(compute_energy_consumption(energy_data, interval))
 # apps store a dataframe value with all related measurements to an app 
 # consumption_per_app stores the sum of all energy measurements of an app
 apps, consumption_per_app = process_app_metrics(data, interval)
-
 
 plt.figure("Data points")
 plt.title("Data points")
@@ -52,3 +60,16 @@ plt.axhline(y=float(tdp), color='r', linestyle='-', label='TDP of 15W')
 plt.legend()
 plt.grid()
 plt.savefig(output_dir + "/energy_data.png")
+
+plt.figure("Power consumption of application " + extra_app)
+plt.title("Power consumption of application " + extra_app)
+plt.plot(transform_timestamp(energy_data['timestamp']), energy_data['consumption'], label="Total power consumption")
+plt.plot(transform_timestamp(apps[extra_app]['timestamp']), apps[extra_app]['consumption'], label=extra_app)
+plt.xlabel("Timestamp")
+plt.ylabel("Power consumption in Watt")
+plt.xticks([transform_timestamp(energy_data['timestamp'])[0], transform_timestamp(energy_data['timestamp'])[len(energy_data)-1]])
+plt.ylim(0, float(tdp)+1.0)
+plt.axhline(y=float(tdp), color='r', linestyle='-', label='TDP of 15W')
+plt.legend()
+plt.grid()
+plt.savefig(output_dir + "/extra_app.png")

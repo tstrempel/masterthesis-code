b0e94ea461bff0f8eddb6b04cdaa50c9a44bd0d0
Tom Strempel <tom.strempel@mailbox.org>
2021-10-09 15:27:51 +0200

    update replication/wrapper.sh

--- replication/wrapper.sh
+++ replication/wrapper.sh
@@ -9,6 +9,7 @@ function run_pipeline {
     # arg6: TDP
     # arg7: threads
     # arg8: application
+    echo ""
     sleep 120
     mkdir "$1"
     scaphandre --no-header json --timeout 86400 --step "$5" --step_nano 0 --max-top-consumers=200 > "$1/energy_data.json" &
@@ -39,45 +40,44 @@ function run_time {
 # $2: threads
 # $3: application, either 'sorting' or 'sorting-O2'
 
-source /scaphandre/init.sh
 
 run_time /data/selection_sort_1000 0 1000 $3
 run_time /data/selection_sort_2000 0 2000 $3
 run_time /data/selection_sort_4000 0 4000 $3
 run_pipeline /data/selection_sort_8000 0 8000 5 1 $1 $2 $3
-run_pipeline selection_sort_16000 0 16000 5 1 $1 $2 $3
-run_pipeline selection_sort_32000 0 32000 5 1 $1 $2 $3
-run_pipeline selection_sort_64000 0 64000 5 1 $1 $2 $3
-run_pipeline selection_sort_128000 0 128000 5 2 $1 $2 $3
+run_pipeline /data/selection_sort_16000 0 16000 5 1 $1 $2 $3
+run_pipeline /data/selection_sort_32000 0 32000 5 1 $1 $2 $3
+run_pipeline /data/selection_sort_64000 0 64000 5 1 $1 $2 $3
+run_pipeline /data/selection_sort_128000 0 128000 5 2 $1 $2 $3
 sleep 120
-run_pipeline selection_sort_256000 0 256000 5 2 $1 $2 $3
-sleep 120
-
-run_time insertion_sort_1000 1 1000 $3
-run_time insertion_sort_2000 1 2000 $3
-run_time insertion_sort_4000 1 4000 $3
-run_pipeline insertion_sort_8000 1 8000 5 1 $1 $2 $3
-run_pipeline insertion_sort_16000 1 16000 5 1 $1 $2 $3
-run_pipeline insertion_sort_32000 1 32000 5 1 $1 $2 $3
-run_pipeline insertion_sort_64000 1 64000 5 1 $1 $2 $3
-run_pipeline insertion_sort_128000 1 128000 5 2 $1 $2 $3
-sleep 120
-run_pipeline insertion_sort_256000 1 256000 5 2 $1 $2 $3
+run_pipeline /data/selection_sort_256000 0 256000 5 2 $1 $2 $3
 sleep 120
 
-run_time quick_sort_32000 2 32000 $3
-run_time quick_sort_64000 2 64000 $3
-run_pipeline quick_sort_128000 2 128000 3 1 $1 $2 $3
-run_pipeline quick_sort_256000 2 256000 3 1 $1 $2 $3
-run_pipeline quick_sort_512000 2 512000 3 1 $1 $2 $3
-run_pipeline quick_sort_1024000 2 1024000 5 1 $1 $2 $3
-run_pipeline quick_sort_2048000 2 2048000 5 1 $1 $2 $3
+run_time /data/insertion_sort_1000 1 1000 $3
+run_time /data/insertion_sort_2000 1 2000 $3
+run_time /data/insertion_sort_4000 1 4000 $3
+run_pipeline /data/insertion_sort_8000 1 8000 5 1 $1 $2 $3
+run_pipeline /data/insertion_sort_16000 1 16000 5 1 $1 $2 $3
+run_pipeline /data/insertion_sort_32000 1 32000 5 1 $1 $2 $3
+run_pipeline /data/insertion_sort_64000 1 64000 5 1 $1 $2 $3
+run_pipeline /data/insertion_sort_128000 1 128000 5 2 $1 $2 $3
+sleep 120
+run_pipeline /data/insertion_sort_256000 1 256000 5 2 $1 $2 $3
 sleep 120
 
-run_time qsort_32000 3 32000 $3
-run_time qsort_64000 3 64000 $3
-run_pipeline qsort_128000 3 128000 3 1 $1 $2 $3
-run_pipeline qsort_256000 3 256000 3 1 $1 $2 $3
-run_pipeline qsort_512000 3 512000 3 1 $1 $2 $3
-run_pipeline qsort_1024000 3 1024000 5 1 $1 $2 $3
-run_pipeline qsort_2048000 3 2048000 5 1 $1 $2 $3
+run_time /data/quick_sort_32000 2 32000 $3
+run_time /data/quick_sort_64000 2 64000 $3
+run_pipeline /data/quick_sort_128000 2 128000 3 1 $1 $2 $3
+run_pipeline /data/quick_sort_256000 2 256000 3 1 $1 $2 $3
+run_pipeline /data/quick_sort_512000 2 512000 3 1 $1 $2 $3
+run_pipeline /data/quick_sort_1024000 2 1024000 5 1 $1 $2 $3
+run_pipeline /data/quick_sort_2048000 2 2048000 5 1 $1 $2 $3
+sleep 120
+
+run_time /data/qsort_32000 3 32000 $3
+run_time /data/qsort_64000 3 64000 $3
+run_pipeline /data/qsort_128000 3 128000 3 1 $1 $2 $3
+run_pipeline /data/qsort_256000 3 256000 3 1 $1 $2 $3
+run_pipeline /data/qsort_512000 3 512000 3 1 $1 $2 $3
+run_pipeline /data/qsort_1024000 3 1024000 5 1 $1 $2 $3
+run_pipeline /data/qsort_2048000 3 2048000 5 1 $1 $2 $3
